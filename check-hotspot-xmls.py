7d3f1b14fc45cdf77c138210e970afe36713f8e5
LindaSt <linda.studer@unifr.ch>
2021-06-18 10:33:17 +0200

    finished hotspot checker

--- check-hotspot-xmls.py
+++ check-hotspot-xmls.py
@@ -1,4 +1,3 @@
-# correct Heather's wrong hotspot xml structure
 # example:
 # <?xml version="1.0"?>
 # <ASAP_Annotations>
@@ -19,37 +18,61 @@
 # 	</AnnotationGroups>
 # </ASAP_Annotations>
 
+import glob
+import fire
 import os
-import numpy as np
-from lxml import etree as ET
-from collections import defaultdict
 import xmltodict
 
-file_path = "Z:\\GRP Dawson\\Hotspot Annotations\\B02.2702_ID_AE1_AE3_CD8.xml"
-output_path = "Z:\\GRP Dawson\\Hotspot Annotations Checked\\B02.2702_ID_AE1_AE3_CD8.xml"
-
-tree = ET.parse(file_path)
-root = tree.getroot()
-filename = os.path.basename(os.path.splitext(file_path)[0])
-
-with open(file_path) as fd:
-    doc = xmltodict.parse(fd.read())
+# file_path = "Z:\\GRP Dawson\\Hotspot Annotations\\B02.2702_ID_AE1_AE3_CD8.xml"
+# output_path = "Z:\\GRP Dawson\\Hotspot Annotations Checked\\B02.2702_ID_AE1_AE3_CD8.xml"
 
 
-if doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@Name"] != "Annotation 0":
-    doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@Name"] = "Annotation 0"
+def check_xml(file_path):
+    with open(file_path) as fd:
+        doc = xmltodict.parse(fd.read())
 
-if doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@PartOfGroup"] != "hotspot":
-    doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@PartOfGroup"] = "hotspot"
+    if len(doc) == 0:
+        print(f'File {file_path} is empty (skipped)!')
+        return
 
-if not doc["ASAP_Annotations"]["AnnotationGroups"]:
-    doc["ASAP_Annotations"]["AnnotationGroups"] = {"Group": {"@Name": "hotspot", "@PartOfGroup": "None",
-                                                             "@Color": "#64FE2E", "@Attributes": None}}
+    # check and if necessary correct the structure
+    if doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@Name"] != "Annotation 0":
+        doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@Name"] = "Annotation 0"
 
-#%%
+    if doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@PartOfGroup"] != "hotspot":
+        doc["ASAP_Annotations"]["Annotations"]["Annotation"]["@PartOfGroup"] = "hotspot"
 
-xml = xmltodict.unparse(doc, pretty=True)
-print(xml)
-#%%
-with open(output_path, "w") as text_file:
-    text_file.write(xml)
+    if not doc["ASAP_Annotations"]["AnnotationGroups"]:
+        doc["ASAP_Annotations"]["AnnotationGroups"] = {"Group": {"@Name": "hotspot", "@PartOfGroup": "None",
+                                                                 "@Color": "#64FE2E", "@Attributes": None}}
+    xml = xmltodict.unparse(doc, pretty=True)
+    return xml
+
+
+def check_hotspots(input_path: str, output_path: str, overwrite: bool = False):
+    if os.path.isdir(input_path):
+        file_list = glob.glob(os.path.join(input_path, "*.xml"))
+    else:
+        file_list = [input_path]
+
+    for file_path in file_list:
+        filename = os.path.basename(file_path)
+        if overwrite and os.path.isfile(os.path.join(output_path, filename)):
+            print(f'File {filename} already exists (skipping file).')
+        else:
+            # print(file_path)
+            xml = check_xml(file_path)
+            with open(os.path.join(output_path, filename), "w") as text_file:
+                text_file.write(xml)
+
+
+if __name__ == '__main__':
+    """
+    INPUT:
+    command line arguments:
+    --input-path: path to the xml hotspot file/folder
+    --output-path: path to the folder, where the corrected xml files should be saved to
+    --overwrite: overwrites existing xml files (default is False)
+    """
+    fire.Fire(check_hotspots)
+
